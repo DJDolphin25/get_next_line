08907ef40ddef78a56ee2ef512ba60539c3d176a
theoppon <theoppon@student.42belgium.be>
2025-12-29 19:56:13 +0100

    Nada

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: theoppon <theoppon@student.42belgium.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/29 17:13:50 by theoppon          #+#    #+#             */
+/*   Updated: 2025/12/29 18:31:29 by theoppon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistdh.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif 
